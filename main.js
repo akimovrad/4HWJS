d79cc01dc349bfc8f4ce9cd11b7486a4a0ee4a7c
akimovrad <raatoaposat@gmail.com>
2024-12-25 16:11:39 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let users = [
+    {id: 1, name: "Вася"},
+    {id: 2, name: "Петя"},
+    {id: 3, name: "Маша"}
+];
+
+let userWithId2 = users.find(user => user.id === 2);
+console.log(userWithId2); 
+
+let filteredUsers = users.filter(user => user.id > 1);
+console.log(filteredUsers); 
